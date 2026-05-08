215e37dbb2e69ac5796426bea851cf10d55ab75b
MaddySingh2003 <Milansuryavanshi27@gmail.com>
2026-05-08 13:00:41 +0530

    links

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Terminal } from "lucide-react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaCode, FaGithub, FaLinkedin, FaStackOverflow, FaTwitter } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -23,6 +24,18 @@ export default function Footer() {
           <div className="flex items-center gap-8">
             <a href="https://github.com/MaddySingh2003" className="text-gray-500 hover:text-ai-neon transition-colors transform hover:scale-125 hover:shadow-[0_0_15px_#00F0FF] rounded-full">
               <FaGithub size={22} />
+            </a>
+
+            <a
+  href="https://leetcode.com/u/_MILAN_SINGH/"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="text-gray-500 hover:text-yellow-400 transition-colors transform hover:scale-125 hover:shadow-[0_0_15px_#FFA116] rounded-full"
+>
+  <SiLeetcode size={22} />
+</a>
+            <a href="https://stackoverflow.com/users/32536879/milan-suryavanshi" className="text-gray-500 hover:text-red-400 transition-colors transform hover:scale-125 hover:shadow-[0_0_15px_#00F0FF] rounded-full">
+              <FaStackOverflow size={22}/>
             </a>
             <a href="https://www.linkedin.com/in/milan-suryavanshi/" className="text-gray-500 hover:text-ai-purple transition-colors transform hover:scale-125 hover:shadow-[0_0_15px_#8A2BE2] rounded-full">
               <FaLinkedin size={22} />
